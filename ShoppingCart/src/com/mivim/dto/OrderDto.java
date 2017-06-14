70152f85ea1384aea341dbf6c22de8a98f9d1461
Shruti9520 <shrutisingh7219@gmail.com>
2017-06-14 16:07:14 +0530

    added dto

---
+++ ShoppingCart/src/com/mivim/dto/OrderDto.java
@@ -0,0 +1,54 @@
+package com.mivim.dto;
+
+public class OrderDto {
+   private String userId;
+   private String subTotal;
+   private String itemId;
+   private String itemName;
+   private String total;
+   private String quantity;
+   private double price;
+   
+   public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+public String getItemName() {
+	return itemName;
+}
+public void setItemName(String itemName) {
+	this.itemName = itemName;
+}
+public String getUserId() {
+	   return userId ;  
+   }
+   public void setUserId(String userId) {
+	   this.userId=userId;
+   }
+   public String getSubTotal() {
+	   return subTotal;
+   }
+   public void setSubTotal(String subTotal) {
+	   this.subTotal= subTotal;
+   }
+   public String getItemId() {
+	   return itemId;
+   }
+   public void setItemId(String itemId) {
+	   this.itemId= itemId;
+   }
+   public String getTotal() {
+	   return total;
+   }
+   public void setTotal(String total) {
+	  this.total= total;
+   }
+   public String getQuantity() {
+	   return quantity;
+   }
+   public void setQuantity(String quantity) {
+	   this.quantity=quantity;
+   }
+}
